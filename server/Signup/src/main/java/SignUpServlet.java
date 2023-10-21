d2fc893a65697a0932ecfb6c9bd4ee7589deafd5
jihyoung <geobrown021@gmail.com>
2023-10-21 21:16:39 +0900

    7 주차 실습과제

---
+++ server/Signup/src/main/java/SignUpServlet.java
@@ -0,0 +1,56 @@
+
+
+import java.io.PrintWriter;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SignUpServlet
+ */
+@WebServlet("/signup")
+public class SignUpServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SignUpServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html;charset=UTF-8");
+		
+		String name = request.getParameter("NAME");
+		String phone = request.getParameter("PHONE");
+		String address = request.getParameter("ADDRESS");
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("<HTML>");
+		out.println("<HEAD></HEAD>");
+		out.println("<BODY>");
+		out.println("NAME = " + name + "<br>");
+		out.println("PHONE = " + phone + "<br>");
+		out.println("ADDRESS = " + address + "<br>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
